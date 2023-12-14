cd5abf32c1faef1980962b8e234e7eff87d8de38
rovannoufer <noufermohamed007@gmail.com>
2023-12-14 23:49:13 +0530

    Second phase completed

--- blog/core/forms.py
+++ blog/core/forms.py
@@ -1,7 +1,19 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from .models import Project
+from django import forms
 
 class BlogForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title','intro','description','tags']
+        fields = ['title','intro', 'featured_image','description','tags']
+        widgets = {
+            'tags' : forms.CheckboxSelectMultiple()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(BlogForm, self).__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add title . . .'})
+        self.fields['intro'].widget.attrs.update({'class': 'input', 'placeholder': 'Add Introduction . . .'})
+        self.fields['description'].widget.attrs.update({'class': 'input', 'placeholder': 'Add Description . . .'})
+       
