dc6353b079cdc9e41ae395d3f503b6145b0b88e4
rovannoufer <noufermohamed007@gmail.com>
2023-12-15 20:30:17 +0530

    signals updated

---
+++ blog/users/signals.py
@@ -0,0 +1,30 @@
+from django.db.models.signals import post_save, post_delete
+
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+
+
+def createProfile(sender, instance, created, **kwargs):
+    print("Signal created")
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+        )
+
+
+def deleteUser(sender, instance, **kwargs):
+    try:
+        user = instance.user
+        user.delete()
+    except:
+        pass
+
+
+post_save.connect(createProfile, sender=User)
+post_delete.connect(deleteUser, sender=Profile)
