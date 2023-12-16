ff130919878a815c8f3db78c7b431e8919e6b565
rovannoufer <noufermohamed007@gmail.com>
2023-12-16 22:08:43 +0530

    gitignore file

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'noufer_1109_',
+        'HOST': 'localhost',
+        'PORT': '5432' 
     }
 }
 
